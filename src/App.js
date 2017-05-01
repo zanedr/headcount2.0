0012e4ed58ce0a8312677fb36ca014dd09cf9d07
Zane Russell <zaneroosa@gmail.com>
2017-05-01 14:56:35 -0600

    iteration zero maybe done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import kinderData from '../data/kindergartners_in_full_day_program.js'
 
 class App extends Component {
+  constructor() {
+    super()
+    data: []
+  }
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>Welcome To Headcount 2.0
+      </div>
     );
   }
 }
