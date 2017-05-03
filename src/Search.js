b0fd53321c38f73b431f608b4697ceb54c342ba3
Zane Russell <zaneroosa@gmail.com>
2017-05-03 14:34:09 -0600

    search function is a go

---
+++ src/Search.js
@@ -0,0 +1,49 @@
+// import React, { Component } from 'react'
+import DistrictRepository from './helper'
+import Card from './Card'
+
+export const searchResults = (input) => {
+  const found = Object.keys(this.state.data).map((location,i)=>{
+    let active =  activeCards[0]==i||activeCards[1]==i?"2px":"0px"
+      let info = this.state.data[location]
+      return(
+        <Card
+        active={active}
+        handleSelectCard={this.selectCard.bind(this)}
+        key={i}
+        index={i}
+        location= {location}
+        info={info} />
+      )
+    })
+  return found
+}
+
+// const results = Object.keys(this.props.data).map((location,i)=>{
+//  let active =  activeCards[0]==i||activeCards[1]==i?"2px":"0px"
+//   let info = this.state.data[location]
+//   return(
+//     <Card
+//     active={active}
+//     handleSelectCard={this.selectCard.bind(this)}
+//     key={i}
+//     index={i}
+//     location= {location}
+//     info={info} />
+//   )
+// })
+
+//   render({data}) {
+//     return(
+//       console.log(data)
+//       <div>
+//         <input className="search-input"
+//                onChange={(e) => {
+//           console.log(e.target.value)
+//         }}/>
+//         {results}
+//         <h4>Search</h4>
+//       </div>
+//     )
+//   }
+// }
